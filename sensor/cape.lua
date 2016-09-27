653f02ef71c6b699e298e7ea4e9094abd4de658c
Anton Gustafsson <anton.gustafsson@tii.se>
2016-09-27 11:52:09 +0200

    Check ip

--- sensor/cape.lua
+++ sensor/cape.lua
@@ -1,2 +1,16 @@
-print("Starting CAPE")
-print(wifi.sta.getip())
+function StartCape()
+
+    print("Starting CAPE")
+    print(wifi.sta.getip())
+    
+
+end
+
+
+if (wifi.sta.getip() ~= nil) then
+    StartCape()
+else
+    wifi.sta.eventMonReg(wifi.STA_GOTIP, StartCape)
+end
+
+
